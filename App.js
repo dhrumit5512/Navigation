834a1b6999af9aa12940ee7d7256e2a266dd92d8
dhrumit <dhrumitbarot1@gmail.com>
2023-03-15 15:31:16 -0600

    link two new create files in app.js.

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
 import * as React from 'react';
-import { Button, Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import {Button, Text, View} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import DetailsScreen from './DetailsScreen';
+import FrogsScreen from './FrogsScreen';
